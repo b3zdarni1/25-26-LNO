43eb4cbcce1161f74d6d90960178d7bb992a6797
unknown <savely.babckin@yandez.ru>
2026-01-16 22:54:26 +0300

    file dzshka2.py added

---
+++ 9 класс/3 четверть/2026-01-20/dzshka2.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+data = [64, 34, 25, 12, 22, 11, 90]
+print("До сортировки:", data)
+sorted_data = insertion_sort(data.copy())
+print("После сортировки:", sorted_data)
