6c47dc3b5df7e4842ba320b8e252e92f1a7ca863
b3zdarni1 <savely.babckin@yandex.ru>
2026-01-22 22:06:03 +0300

    Create dzshka8.py

---
+++ 9 класс/3 четверть/2026-01-23/dzshka8.py
@@ -0,0 +1,4 @@
+text = "Это пример текста. Этот текст состоит из нескольких слов." 
+unique_symvol = set(text)
+sorted_unique_symvol= sorted(unique_symvol) 
+print("Уникальные символы:", sorted_unique_symvol)
